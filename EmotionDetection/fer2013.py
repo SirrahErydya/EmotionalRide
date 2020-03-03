bbb41e26342b01b2eb308fc81a34692c97f0a0e2
SirrahErydya <bo231@stud.uni-heidelberg.de>
2020-03-03 18:01:54 +0100

    Basic network and data prep

---
+++ EmotionDetection/fer2013.py
@@ -0,0 +1,39 @@
+import os
+import csv
+from torch.utils.data import Dataset
+import numpy as np
+
+
+class FER2013(Dataset):
+    def __init__(self, root, train=True, transform=None):
+        self.train = train
+        if self.train:
+            path = os.path.join(root, 'train.csv')
+        else:
+            path = os.path.join(root, 'test.csv')
+        self.transform = transform
+        self.data = []
+        self.targets = []
+        self.make_dataset(path)
+        self.labels = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
+
+    def __getitem__(self, item):
+        img = self.data[item]
+        if self.transform is not None:
+            img = self.transform(img)
+        if self.train:
+            return img, self.targets[item]
+        return img
+
+    def __len__(self):
+        return len(self.data)
+
+    def make_dataset(self, path):
+        with open(path) as file:
+            reader = csv.DictReader(file, delimiter=',')
+            for row in reader:
+                image = np.fromstring(row['pixels'], dtype=np.float32, sep=' ').reshape((1, 48, 48))
+                if self.train:
+                    self.targets.append(int(row['emotion']))
+                self.data.append(image)
+
---
+++ EmotionDetection/network.py
@@ -0,0 +1,32 @@
+"""
+A deep CNN to classify the emotions in a human face.
+Architecture inspired by https://arxiv.org/pdf/1910.05602.pdf
+:author: Fenja Kollasch
+"""
+import torch.nn as nn
+
+
+class EmotionClassifier(nn.Module):
+    def __init__(self):
+        super(EmotionClassifier, self).__init__()
+        self.convolutions = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=64, kernel_size=3, stride=1), nn.ReLU(),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1), nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2), nn.Dropout(0.25),
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1), nn.ReLU(),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1), nn.ReLU(),
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1), nn.ReLU(),
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1), nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),  nn.Dropout(0.25),
+            nn.Flatten()
+        )
+        self.fc1 = nn.Linear(in_features=20736, out_features=1024)
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(p=0.5)
+        self.output_layer = nn.Linear(in_features=1024, out_features=7)
+        self.softmax = nn.Softmax()
+
+    def forward(self, x):
+        x = self.convolutions(x)
+        x = self.dropout(self.relu((self.fc1(x))))
+        return self.softmax(self.output_layer(x))

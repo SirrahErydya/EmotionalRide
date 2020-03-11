e6ad14ca8abb3261a7f5df499e136cd2aa3ff161
SirrahErydya <bo231@stud.uni-heidelberg.de>
2020-03-11 14:44:21 +0100

    More data

--- EmotionDetection/data.py
+++ EmotionDetection/data.py
@@ -8,21 +8,19 @@ from PIL import Image
 LABELS = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
 
 
-class FER2013(Dataset):
+class FerDataset(Dataset):
     def __init__(self, root, train=True, transform=None, reduce_emotions=False):
         self.train = train
-        if self.train:
-            path = os.path.join(root, 'train.csv')
-        else:
-            path = os.path.join(root, 'test.csv')
+        self.root = root
         self.transform = transform
         self.data = []
         self.targets = []
         self.reduce_emotions = reduce_emotions
-        self.make_dataset(path)
 
     def __getitem__(self, item):
         img = self.data[item]
+        if self.transform is not None:
+            img = self.transform(img)
         if self.train:
             return img, self.targets[item]
         return img
@@ -30,7 +28,20 @@ class FER2013(Dataset):
     def __len__(self):
         return len(self.data)
 
-    def make_dataset(self, path):
+    def make_dataset(self):
+        raise NotImplementedError('Implement in Subclasses')
+
+
+class FER2013(FerDataset):
+    def __init__(self, root, train=True, transform=None, reduce_emotions=False):
+        super(FER2013, self).__init__(root, train, transform, reduce_emotions)
+        self.make_dataset()
+
+    def make_dataset(self):
+        if self.train:
+            path = os.path.join(self.root, 'train.csv')
+        else:
+            path = os.path.join(self.root, 'test.csv')
         with open(path) as file:
             reader = csv.DictReader(file, delimiter=',')
             for row in reader:
@@ -42,56 +53,85 @@ class FER2013(Dataset):
                             emotion = emotion - 1
                         assert emotion < 6
                     self.targets.append(emotion)
-                    if self.transform is not None:
-                        self.targets.append(emotion)
-                self.data.append(ToTensor()(image))
-                if self.transform is not None:
-                    self.data.append(self.transform(image))
+                self.data.append(image)
 
 
-class CKPlus(Dataset):
+class CKPlus(FerDataset):
     def __init__(self, root, transform=None, reduce_emotions=False):
-        self.path = os.path.join(root, "CK+48")
-        self.transform = transform
-        self.data = []
-        self.targets = []
-        self.reduce_emotions = reduce_emotions
-        self.emotions = ['anger', 'disgust', 'fear', 'happy', 'sadness', 'surprise', 'contempt']
+        super(CKPlus, self).__init__(root, transform=transform, reduce_emotions=reduce_emotions)
+        self.emotions = ['anger', 'disgust', 'fear', 'happy', 'sadness', 'surprise']
+        self.make_dataset()
+
+    def make_dataset(self):
         for i in range(len(self.emotions)):
             self.get_data(i)
 
-    def __getitem__(self, item):
-        img = self.data[item]
-        return img, self.targets[item]
-
-    def __len__(self):
-        return len(self.data)
-
     def get_data(self, target):
-        img_folder = os.path.join(self.path, self.emotions[target])
+        path = os.path.join(self.root, "CK+48")
+        img_folder = os.path.join(path, self.emotions[target])
         if self.reduce_emotions and target > 0:
             target = target - 1
         for img_file in os.listdir(img_folder):
             if img_file.endswith('.png'):
                 pil_img = Image.open(os.path.join(img_folder, img_file))
                 img = np.array(pil_img, dtype=np.float32)
-                self.data.append(ToTensor()(img))
+                self.data.append(img)
                 self.targets.append(np.int32(target))
-                if self.transform is not None:
-                    self.data.append(self.transform(img))
-                    self.targets.append(np.int32(target))
+
+
+class FacialExpression(FerDataset):
+    def __init__(self, root, transform=None, reduce_emotions=False):
+        super(FacialExpression, self).__init__(root, transform=transform, reduce_emotions=reduce_emotions)
+        self.emo_map = {'anger': 0, 'disgust': 1, 'fear': 2, 'happiness': 3, 'sadness': 4, 'surprise': 5, 'neutral': 6}
+        if self.reduce_emotions:
+            self.emo_map = {'anger': 0, 'disgust': 0, 'fear': 1, 'happiness': 2, 'sadness': 3, 'surprise': 4,
+                            'neutral': 5}
+        self.make_dataset()
+
+    def make_dataset(self):
+        legend_path = os.path.join(self.root, 'data', 'legend.csv')
+        img_path = os.path.join(self.root, 'images')
+        with open(legend_path) as file:
+            reader = csv.DictReader(file, delimiter=',')
+            for row in reader:
+                emotion = row['emotion'].lower()
+                try:
+                    self.targets.append(np.int32(self.emo_map[emotion]))
+                    pil_img = Image.open(os.path.join(img_path, row['image']))
+                    pil_img = pil_img.resize((48,48), Image.BICUBIC)
+                    pil_img = pil_img.convert('L')
+                    img = np.array(pil_img, dtype=np.float32)
+                    self.data.append(img)
+                except KeyError:
+                    print("Skipping unknown emotion:", emotion)
 
 
 class CombiDataset(Dataset):
-    def __init__(self, fer_root, ck_root, transform=None, reduce_emotions=False):
-        self.transform = transform
+    def __init__(self, fer_root, ck_root, fe_root, transform=None, reduce_emotions=False):
         fer = FER2013(fer_root, transform=transform, reduce_emotions=reduce_emotions)
         ck = CKPlus(ck_root, transform=transform, reduce_emotions=reduce_emotions)
-        self.data = fer.data + ck.data
-        print("Combined dataset: FER2013 and CKPlus")
+        fe = FacialExpression(fe_root, transform=transform, reduce_emotions=reduce_emotions)
+        self.data = fer.data + ck.data + fe.data
+        self.targets = fer.targets + ck.targets + fe.targets
+        if transform is not None:
+            max_length = np.max([len(fer.data), len(ck.data), len(fe.data)])
+            for i in range(max_length):
+                if i < len(fer):
+                    data, target = fer[i]
+                    self.data.append(data)
+                    self.targets.append(target)
+                if i < len(ck):
+                    data, target = ck[i]
+                    self.data.append(data)
+                    self.targets.append(target)
+                if i < len(fe):
+                    data, target = fe[i]
+                    self.data.append(data)
+                    self.targets.append(target)
+        print("Combined dataset:")
         print("FER Samples:", len(fer.data))
         print("CKPlus samples:", len(ck.data))
-        self.targets = fer.targets + ck.targets
+        print("Facial expression samples:", len(fe.data))
 
     def __getitem__(self, item):
         img = self.data[item]

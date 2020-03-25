78ef80031a85ce5f94328b19a80e9949215c3e9d
SirrahErydya <bo231@stud.uni-heidelberg.de>
2020-03-25 10:32:35 +0100

    It works!!!

--- EmotionDetection/main.py
+++ EmotionDetection/main.py
@@ -4,6 +4,6 @@ from network import EmotionClassifier
 from perception import track_webcam
 
 if __name__ == '__main__':
-    emo = EmotionClassifier(out_layers=6)
+    emo = EmotionClassifier(out_layers=4)
     emo.load_state_dict(torch.load('models/model-light.pt', map_location=torch.device('cpu')))
     track_webcam(emo)
--- EmotionDetection/perception.py
+++ EmotionDetection/perception.py
@@ -1,28 +1,25 @@
 import cv2
 import torch
 
-LABELS = ['Angry/Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
+LABELS = ['Angry', 'Happy', 'Sad', 'Surprise']
 
 
 def track_webcam(classifier, key_frame=20):
     video_capture = cv2.VideoCapture(0)
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
     frame_counter = 0
     while True:
         if frame_counter == key_frame:
             frame_counter = 0
         ret, frame = video_capture.read()
-        height, width, _ = frame.shape
-        obsolete_pxls = width - height
-        crop = frame[:, int(obsolete_pxls / 2): width - int(obsolete_pxls / 2)]
-        big_picture = cv2.resize(crop, (1500, 1500))
-        if frame_counter == 0:
-            grayscale = cv2.cvtColor(crop, cv2.COLOR_BGR2GRAY)
-            resized = cv2.resize(grayscale, (48, 48))
+        face = extract_face(face_cascade, frame)
+        if frame_counter == 0 and face is not None:
+            resized = cv2.resize(face, (48, 48))
             tensor = torch.Tensor(resized)
             prediction = classifier(tensor).flatten()
-        display_prediction(prediction, big_picture)
-        cv2.imshow('Video', big_picture)
-        cv2.imshow("Control Image", resized)
+            cv2.imshow("Face", face)
+        display_prediction(prediction, frame)
+        cv2.imshow('Video', frame)
         frame_counter += 1
         # Quit with 'q'
         if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -37,7 +34,24 @@ def display_prediction(emo_prediction, frame):
     emos = torch.argsort(emo_prediction, descending=True)
     text_vert = 30
     for emo in emos:
-        percentage = (emo_prediction[emo].item())
-        text = "{0}: {1}".format(LABELS[emo.item()], percentage)
+        percentage = torch.exp(emo_prediction[emo]).item() * 100
+        text = "{0}: {1:.2f}".format(LABELS[emo.item()], percentage, '.2f')
         cv2.putText(frame, text, (50, text_vert), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0, 255), 3)
         text_vert += 30
+
+
+def extract_face(face_cascade, frame):
+    grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(
+        grayscale,
+        scaleFactor=1.3,
+        minNeighbors=3,
+        minSize=(48, 48)
+    )
+    if len(faces) > 0:
+        x, y, w, h = faces[0]
+        if w > 0 and h > 0:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            face = frame[y:y+h, x:x+w]
+            return cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
+    return None
